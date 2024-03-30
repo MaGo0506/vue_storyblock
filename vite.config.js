71c0785486990dc38436c00068dabb9178b79584
Marko Goronjic <goronjicmarko24@gmail.com>
2024-03-30 21:00:18 +0100

    - setting up https protocol

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    mkcert()
   ],
+  server: {
+    https: true,
+    port: 3000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
