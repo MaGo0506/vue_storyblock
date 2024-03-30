37ecbe8bf158303e13c0d476d03aebb52b74730f
Marko Goronjic <goronjicmarko24@gmail.com>
2024-03-30 21:07:42 +0100

    - vue storyblock init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { StoryblokVue, apiPlugin } from '@storyblok/vue';
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
@@ -10,5 +11,13 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(StoryblokVue, {
+    accessToken: '6rKKLFrzBkjonzueYwWuMQtt',
+    bridge: process.env.NODE_ENV !== 'production', // optimizes by excluding the bridge on production
+    use: [apiPlugin],
+    apiOptions: {
+        region: "eu",
+    },
+});
 
 app.mount('#app')
